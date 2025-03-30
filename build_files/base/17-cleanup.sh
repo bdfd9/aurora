9008a4329da126f854c7402c21898d4d8c000bc0
bdfd9 <dpaprocki+github@proton.me>
2025-03-30 17:53:08 +0200

    Update 17-cleanup.sh

--- build_files/base/17-cleanup.sh
+++ build_files/base/17-cleanup.sh
@@ -19,7 +19,6 @@ sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/negativo17-fedora-multimedia.r
 # Disable coprs
 dnf5 -y copr disable ublue-os/staging
 dnf5 -y copr disable ublue-os/packages
-dnf5 -y copr disable phracek/PyCharm
 dnf5 -y copr disable bieszczaders/kernel-cachyos-addons
 dnf5 -y copr disable gmaglione/podman-bootc
 
