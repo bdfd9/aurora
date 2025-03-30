1402ba554d9d852318c3925b683a7856ec36bb11
bdfd9 <dpaprocki+github@proton.me>
2025-03-30 17:38:53 +0200

    Remove tailscale

--- build_files/base/17-cleanup.sh
+++ build_files/base/17-cleanup.sh
@@ -15,7 +15,6 @@ systemctl enable check-sb-key.service
 
 # Disable all COPRs and RPM Fusion Repos and terra
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/negativo17-fedora-multimedia.repo
-sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/tailscale.repo
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/charm.repo
 
 # Disable coprs
